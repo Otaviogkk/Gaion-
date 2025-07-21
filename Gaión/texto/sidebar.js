998b20e2facf1d35ce98e1394dd5fd1c1a47d1d8
Otaviogkk <joseotaviolucianosousachagas@gmail.com>
2025-07-20 23:05:40 -0300

    c

---
+++ Gaión/texto/sidebar.js
@@ -0,0 +1,20 @@
+function perfil() {
+  const sb = document.getElementById('sidebar');
+  sb.classList.add('open');
+}
+
+function fecharPerfil() {
+  const sb = document.getElementById('sidebar');
+  sb.classList.remove('open');
+}
+
+function Sabia_mais() {
+  window.location.href = '../texto/pagina_de_infomação.html';
+}
+
+function Entra() {
+  window.location.href = '../Conta/Gaión.html';
+}
+function volta(){
+  window.location.href ="../inicio/tela-Iniciar.html";
+}
